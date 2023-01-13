e218db37bfb0f43522343a9c5ef72e5ef3f8a83c
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-01-13 09:43:30 +0300

    Revert "drop down menu working but not well aligned"
    This reverts commit 8bcadc74b759b9d55df845b3f98bebf00b8cd86c.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'RecipeBook')</title>
+    <title>@yield('title', 'Favebuk')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
 
     <style>
         body {
@@ -17,36 +18,44 @@
             font: sans-serif;
             font-weight: 200;
             height: 100vh;
-            margin: auto;
+            margin: 0;
         }
 
         .dropdown-content {
+            display: none;
             position: absolute;
             background-color: #f9f9f9;
             min-width: 160px;
             box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            display: none;
-        }
-
-
-        .dropbtn {
-            background-color: transparent;
-            color: #adb3b8;
-            border: none;
-            position: relative;
-        }
-
-        .dropdown-content a {
-            color: black;
-            padding: 12px 16px;
-            text-decoration: none;
-            display: block;
+            z-index: 1;
         }
 
         .dropdown:hover .dropdown-content {
             display: block;
         }
 
+        .dropdown {
+            float: left;
+            overflow: hidden;
+        }
+
+        .dropdown .dropbtn {
+            font-size: 13px;
+            font-weight: 600;
+            color: #636b6f;
+            letter-spacing: .1rem;
+            padding: 0 25px;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
 
         .dropdown-content a:hover {
             background-color: #ddd;
@@ -55,9 +64,9 @@
 </head>
 
 <body>
-
+    <!-- Navigation Bar -->
     <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <a class="navbar-brand" href="">RecipeBook</a>
+        <a class="navbar-brand" href="">Favebuk</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -73,20 +82,22 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact Us</a>
                 </li>
-
-
+                <div class="top-right links">
+                    <div class="dropdown">
+                        <button class="dropbtn">More
+                            <i class="fa fa-caret-down"></i>
+                        </button>
+                        <div class="dropdown-content">
+                            <a href="#">Link 1</a>
+                            <a href="#">Link 2</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
             </ul>
-            <div class="dropdown" end-0>
-                <button class="dropbtn">More</button>
-                <div class="dropdown-content">
-                    <a href="#">My Profile</a>
-                    <a href="#">Logout</a>
-                </div>
-            </div>
         </div>
     </nav>
     </div>
-    <h1>RecipeBook - @yield('title')</h1>
+    <h1>Favebuk - @yield('title')</h1>
     <div>
         @yield('content')
     </div>
