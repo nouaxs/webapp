e6f82676904e9d17d7431b4608085e29c34b5606
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-01-13 10:33:56 +0300

    navigation bar fully working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,11 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'Favebuk')</title>
+    <title>@yield('title', 'RecipeBook')</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
 
     <style>
         body {
@@ -18,44 +17,38 @@
             font: sans-serif;
             font-weight: 200;
             height: 100vh;
-            margin: 0;
+            margin: auto;
         }
 
         .dropdown-content {
-            display: none;
             position: absolute;
             background-color: #f9f9f9;
             min-width: 160px;
             box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
-            z-index: 1;
+            display: none;
+            right: 0;
+        }
+
+
+        .dropdown .dropbtn {
+            background-color: transparent;
+            color: #adb3b8;
+            border: none;
+            position: relative;
+            cursor: pointer;
+        }
+
+        .dropdown-content a {
+            color: black;
+            padding: 12px 16px;
+            text-decoration: none;
+            display: block;
         }
 
         .dropdown:hover .dropdown-content {
             display: block;
         }
 
-        .dropdown {
-            float: left;
-            overflow: hidden;
-        }
-
-        .dropdown .dropbtn {
-            font-size: 13px;
-            font-weight: 600;
-            color: #636b6f;
-            letter-spacing: .1rem;
-            padding: 0 25px;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .position-ref {
-            position: relative;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
 
         .dropdown-content a:hover {
             background-color: #ddd;
@@ -64,9 +57,9 @@
 </head>
 
 <body>
-    <!-- Navigation Bar -->
+
     <nav class="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <a class="navbar-brand" href="">Favebuk</a>
+        <a class="navbar-brand" href="">RecipeBook</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -82,22 +75,26 @@
                 <li class="nav-item">
                     <a class="nav-link" href="/contact">Contact Us</a>
                 </li>
-                <div class="top-right links">
-                    <div class="dropdown">
-                        <button class="dropbtn">More
-                            <i class="fa fa-caret-down"></i>
-                        </button>
-                        <div class="dropdown-content">
-                            <a href="#">Link 1</a>
-                            <a href="#">Link 2</a>
-                            <a href="#">Link 3</a>
-                        </div>
-                    </div>
             </ul>
+
+            
+
+
         </div>
+        <ul class="nav justify-content-end">
+            <li class="nav-item dropdown">
+                <div class="dropbtn" id="navbarDropdown" role="button" data-bs-toggle="dropdown-content"
+                    aria-expanded="false"> More </div>
+                <div class="dropdown-content" aria-labelledby="navbarDropdown">
+                    <a href="#">My Profile</a>
+                    <a href="#">Logout</a>
+                </div>
+            </li>
+        
+    </ul>
     </nav>
     </div>
-    <h1>Favebuk - @yield('title')</h1>
+    <h1>RecipeBook - @yield('title')</h1>
     <div>
         @yield('content')
     </div>
