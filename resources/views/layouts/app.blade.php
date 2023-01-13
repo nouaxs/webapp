cf284186359b272ae1f21db7c45be66de55bc018
nouaxs <74322389+nouaxs@users.noreply.github.com>
2023-01-13 21:56:45 +0300

    sign in page layout done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>@yield('title', 'RecipeBook')</title>
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
         integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
@@ -53,14 +51,21 @@
         .dropdown-content a:hover {
             background-color: #ddd;
         }
+
+        .center {
+            display: flex;
+            justify-content: center;
+            padding: 50px 0;
+        }
     </style>
 </head>
 
 <body>
     @include('includes.header')
-    <h1>RecipeBook - @yield('title')</h1>
-    <div>
+    <div class="center">
+    <div class="col-md-6">
         @yield('content')
+    </div>
     </div>
 </body>
 
